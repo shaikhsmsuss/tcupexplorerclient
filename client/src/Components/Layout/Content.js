b8c8b255313a01c3a912141bc44ce3f2cb88ce45
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2019-03-18 16:39:36 +0530

    initial commit

--- client/src/Components/Layout/Content.js
+++ client/src/Components/Layout/Content.js
@@ -11,9 +11,7 @@ export class Content extends Component {
                 <div className="col-lg-8 p-r-0 title-margin-right">
                   <div className="page-header">
                     <div className="page-title">
-                      <h1>
-                        Hello, <span>Welcome Here</span>
-                      </h1>
+                      <h1>Hello,Welcome Here</h1>
                     </div>
                   </div>
                 </div>
@@ -30,42 +28,127 @@ export class Content extends Component {
                   </div>
                 </div>
               </div>
-              <section id="main-content">
-                <div className="container">
-                  <div className="row">
-                    <div class="col-sm-4">
-                      <i class="fa fa-folder  fa-2x" aria-hidden="true">
-                        <p className="text">folder2</p>
-                      </i>
-                    </div>
-                    <div class="col-sm-4">
-                      <i class="fa fa-folder fa-2x" aria-hidden="true">
-                        <p className="text">folder2</p>
-                      </i>
-                    </div>
-                    <div class="col-sm-4">
-                      <i class="fa fa-folder  fa-2x" aria-hidden="true">
-                        <p className="text">folder2</p>
-                      </i>
-                    </div>
-                    <div class="col-sm-4">
-                      <i class="fa fa-folder  fa-2x" aria-hidden="true">
-                        <p className="text">folder2</p>
-                      </i>
-                    </div>
-                    <div class="col-sm-4">
-                      <i class="fa fa-folder  fa-2x" aria-hidden="true">
-                        <p className="text">folder2</p>
-                      </i>
-                    </div>
-                    <div class="col-sm-4">
-                      <i class="fa fa-folder " aria-hidden="true">
-                        <p className="text">folder2</p>
-                      </i>
+              <div class="content">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="card" />
+                      <div class="content all-icons">
+                        <div class="row1">
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder1" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder2" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder3" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                          <div class="font-icon-list col-lg-2 col-md-3 col-sm-4 col-xs-6 col-xs-6">
+                            <div class="font-icon-detail">
+                              <i
+                                class="fa fa-folder  fa-3x"
+                                aria-hidden="true"
+                              />
+                              <input type="text" value="folder4" />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </section>
+              </div>
             </div>
           </div>
         </div>
